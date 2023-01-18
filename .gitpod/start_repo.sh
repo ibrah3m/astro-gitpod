48e875f013e0b859ef91324a32f69b10a826bc52
ibrah3m <91458548+ibrah3m@users.noreply.github.com>
2023-01-18 17:27:10 +0300

    Update start_repo.sh

--- .gitpod/start_repo.sh
+++ .gitpod/start_repo.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
+reponame="ASTRO_REPO"
+if [ "$TASK" == "init" ]; then
+# Set up a given ASTRO_REPO repository in gitpod (default to astro template)
+export ASTRO_REPO=${ASTRO_REPO:-https://github.com/astro/template}
+echo "Checking out repository ${ASTRO_REPO}"
 
-set -eu
-
-# Set up a given DDEV_REPO repository in gitpod (default to d9simple)
-export ASTRO_REPO=${ASTRO_REPO:-https://github.com/drud/d9simple}
-echo "Checking out repository ${DDEV_REPO}"
-reponame=${ASTRO_REPO##*/}
-reponame="${reponame//_/-}"
 git clone ${ASTRO_REPO} ${GITPOD_REPO_ROOT}/${reponame}
 if [ -d ${GITPOD_REPO_ROOT}/${reponame} ]; then
   cd ${GITPOD_REPO_ROOT}/${reponame}
-
-
+  pnpm install
+  pnpm run build
+    fi
+    fi
+    
+    
+if [ "$(pwd)" != ${GITPOD_REPO_ROOT}/${reponame} ]; then
+    cd ${GITPOD_REPO_ROOT}/${reponame}
+fi
+    
 # Wait for VSCode to be ready (port 23000)
 gp ports await 23000 > /dev/null 2>&1
 
 # Open the main page in VSCode
 code src/pages/index.astro
-#check if there's need to install the libraries ?
+#check if triggerd by init task from gitpod.yml
 
 # Start Astro
 pnpm start
